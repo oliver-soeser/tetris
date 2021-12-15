6abd91d7546047ff85e4479ee8c4948e8d838571
Oliver <oliver@MacBookPro16-SS.local>
2021-12-15 10:05:57 +0100

    Game logic without collision

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ let canvas = null;
 let ctx = null;
 
 cw = 672;
-ch = 864;
+ch = 864-48;
 
 stdColor = "#222222";
 
 let map = [];
+let score = 0;
 
 blocks = [
   [
@@ -165,11 +166,32 @@ function startGame() {
 
 function dropMainBlock() {
   let obj = readObject(mainBlockId);
-  obj.y += 1;
 
-  if (checkPossibleMoveY(obj.x, obj.y, obj.shape)) {
+  if (checkPossibleMoveY(obj.x, obj.y+1, obj.shape)) {
+    obj.y += 1;
     writeObject(mainBlockId, obj);
   } else {
+    // Check if any lines are full
+    let linesFull = 0;
+    for (let i = 0; i < ch / tileSize; i++) {
+      let full = true;
+      for (let j = 0; j < map.length; j++) {
+        if (map[j][i] == stdColor) {
+          full = false;
+          break;
+        }
+      }
+      if (full) {
+        linesFull++;
+        for (let k = 0; k < objects.length; k++) {
+          objects[k].y += 1;
+        }
+      }
+    }
+    if (linesFull > 0) {
+      score += linesFull * 100;
+      document.getElementById("score").innerHTML = "Score: "+score;
+    }
     newMainBlock();
   }
 }
@@ -178,20 +200,48 @@ function checkPossibleMoveY(x, y, shape) {
   for (let i = 0; i < shape.length; i++) {
     for (let j = 0; j < shape[i].length; j++) {
       if (shape[i][j] == 1) {
-        if (y + j >= map[x + i].length - 1) {
+        if (y + j >= ch / tileSize) {
           return false;
         }
-        for (let k = 0; k < objects.length - 1; k++) {
-          for (let ki = 0; ki < objects[k].shape.length; ki++) {
-            for (let kj = 0; kj < objects[k].shape[ki].length; kj++) {
-              if (objects[k].shape[ki][kj] == 1) {
-                if (objects[k].x == x + i && objects[k].y == y + j) {
-                  return false;
-                }
-              }
-            }
-          }
+      }
+    }
+  }
+  let cmaskA = [];
+  for (let i = 0; i < cw / tileSize; i++) {
+    cmaskA[i] = [];
+    for (let j = 0; j < ch / tileSize; j++) {
+      cmaskA[i][j] = 0;
+    }
+  }
+  for (let k = 0; k < objects.length - 1; k++) {
+    for (let ki = 0; ki < objects[k].shape.length; ki++) {
+      for (let kj = 0; kj < objects[k].shape[ki].length; kj++) {
+        if (objects[k].shape[ki][kj] == 1) {
+          cmaskA[objects[k].x + ki, objects[k].y + kj] = 1;
         }
+      }
+    }
+  }
+  let cmaskB = [];
+  for (let i = 0; i < cw / tileSize; i++) {
+    cmaskB[i] = [];
+    for (let j = 0; j < ch / tileSize; j++) {
+      cmaskB[i][j] = 0;
+    }
+  }
+  for (let i = 0; i < shape.length; i++) {
+    for (let j = 0; j < shape[i].length; j++) {
+      if (shape[i][j] == 1) {
+        cmaskB[x + i][y + j] = 1;
+      }
+    }
+  }
+  console.log(cmaskA);
+  console.log(cmaskB);
+  for (let i = 0; i < cw / tileSize; i++) {
+    for (let j = 0; j < ch / tileSize; j++) {
+      if (cmaskA[i][j] == 1 && cmaskB[i][j] == 1) {
+        return false;
       }
     }
   }
@@ -201,19 +251,57 @@ function checkPossibleMoveY(x, y, shape) {
 document.addEventListener("keydown", function (event) {
   if (event.keyCode == 37) {
     let obj = readObject(mainBlockId);
-    obj.x -= 1;
-    writeObject(mainBlockId, obj);
+    let lowestShapeX = obj.shape.length;
+    for (let i = 0; i < obj.shape.length; i++) {
+      for (let j = 0; j < obj.shape[i].length; j++) {
+        if (obj.shape[i][j] == 1) {
+          if (lowestShapeX > i) {
+            lowestShapeX = i;
+          }
+        }
+      }
+    }
+    if (obj.x+lowestShapeX > 0) {
+      obj.x -= 1;
+      writeObject(mainBlockId, obj);
+    }
   } else if (event.keyCode == 39) {
     let obj = readObject(mainBlockId);
-    obj.x += 1;
-    writeObject(mainBlockId, obj);
+    let highestShapeX = 0;
+    for (let i = 0; i < obj.shape.length; i++) {
+      for (let j = 0; j < obj.shape[i].length; j++) {
+        if (obj.shape[i][j] == 1) {
+          if (highestShapeX < i) {
+            highestShapeX = i;
+          }
+        }
+      }
+    }
+    if (obj.x+highestShapeX < cw / tileSize - 1) {
+      obj.x += 1;
+      writeObject(mainBlockId, obj);
+    }
   } else if (event.keyCode == 40) {
     dropMainBlock();
   } else if (event.keyCode == 38) {
     let obj = readObject(mainBlockId);
-    obj.shape = rotateShape(obj.shape);
-    writeObject(mainBlockId, obj);
+    let newShape = rotateShape(obj.shape);
+    let validRot = true;
+    for (let i = 0; i < newShape.length; i++) {
+      for (let j = 0; j < newShape[i].length; j++) {
+        if (newShape[i][j] == 1) {
+          if (obj.x + i < 0 || obj.x + i >= cw / tileSize || obj.y + j >= ch / tileSize) {
+            validRot = false;
+            break;
+          }
+        }
+      }
+    }
+    if (validRot) {
+      obj.shape = newShape;
+      writeObject(mainBlockId, obj);
+    }
   }
 });
 
-// TODO : Collision, Score, UI
+// TODO: Collision detection
